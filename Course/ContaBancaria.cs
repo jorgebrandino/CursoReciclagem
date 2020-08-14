67feb92266a60a2ae29943a92be321244cd22278
jorgebrandino <jorge@duasmentes.com.br>
2020-08-14 09:35:02 -0300

    Curso de reciclagem START

---
+++ Course/ContaBancaria.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Course
+{
+    class ContaBancaria
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Nome},  Saldo $ {Saldo.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            Saldo = Saldo + saldo;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos serão alugados? ");
+            int room = int.Parse(Console.ReadLine());
+
+            Booking[] vect = new Booking[10];
+
+            for (int i = 0; i < room; i++)
+            {
+                Console.WriteLine("Aluguel #{0}:", i + 1);
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                vect[roomNumber] = new Booking { Name = name, Email = email };
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                    Console.WriteLine(i.ToString() + ": " + vect[i].ToString());
+            }
+        }
+    }
+}
